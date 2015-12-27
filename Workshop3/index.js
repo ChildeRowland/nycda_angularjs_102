9059005d240f37637a6312b7286f2ded4790f8c4
Christopher Mehmed <chrismehmed@gmail.com>
2015-12-26 19:57:25 -0500

    WS3 generate playlist

--- Workshop3/index.js
+++ Workshop3/index.js
@@ -8,7 +8,7 @@ angular.module('playListApp', [])
 		//this.playList = []
 	}
 
-	User.prototype.update = function(name, age) {
+	User.prototype.addUser = function(name, age) {
 		this.name = name;
 		this.age = age;
 		console.log(this.name, this.age);
@@ -23,21 +23,21 @@ angular.module('playListApp', [])
 		this.masterList = [
 			{ genre: "Rock",
 				songs: [
-					{ title: "rock1", type: "nonexplicit", selected: false },
-					{ title: "rock2", type: "explicit", selected: false },
-					{ title: "rock3", type: "explicit", selected: false }
+					{ title: "rock1", type: "nonexplicit" },
+					{ title: "rock2", type: "explicit" },
+					{ title: "rock3", type: "explicit" }
 			]}, 
 			{ genre: "Pop",
 				songs: [
-					{ title: "Pop1", type: "explicit", selected: false },
-					{ title: "Pop2", type: "nonexplicit", selected: false },
-					{ title: "Pop3", type: "explicit", selected: false }
+					{ title: "Pop1", type: "explicit" },
+					{ title: "Pop2", type: "nonexplicit" },
+					{ title: "Pop3", type: "nonexplicit" }
 			]},
 			{ genre: "Talk",
 				songs: [
-					{ title: "Talk1", type: "nonexplicit", selected: false },
-					{ title: "Talk2", type: "explicit", selected: false },
-					{ title: "Talk3", type: "explicit", selected: false }
+					{ title: "Talk1", type: "nonexplicit" },
+					{ title: "Talk2", type: "explicit" },
+					{ title: "Talk3", type: "nonexplicit" }
 			]}
 		]
 	}
@@ -50,6 +50,44 @@ angular.module('playListApp', [])
 		return array;
 	}
 
+	Music.prototype.addSong = function() {
+		array = [];
+		for ( index in this.masterList ) {
+			
+			for ( idx in this.masterList[index].songs ) {
+
+				if ( this.masterList[index].songs[idx].selected === true ) {
+					array.push( this.masterList[index].songs[idx] );
+				}
+			}
+		}
+		for ( index in array ) {
+			delete array[index].selected;
+		}
+		console.log(array);
+		return array;
+	}
+
+	Music.prototype.isNonExplicit = function(index, idx, lyricType) {
+		var songObject = this.masterList[index].songs[idx]
+		if ( songObject.type === "nonexplicit" || 
+			( songObject.type === "explicit" && lyricType === "explicit" ) ) {
+			return true;
+		}
+	}
+
+	Music.prototype.removeExplicit = function() {
+		for ( index in this.masterList) {
+			for ( idx in this.masterList[index].songs ) {
+				if ( this.masterList[index].songs[idx].selected === true && 
+			     	 this.masterList[index].songs[idx].type === "explicit" ) { 
+
+						this.masterList[index].songs[idx].selected = false;
+				}
+			}
+		}
+	}
+
 	return Music;
 
 })
@@ -59,18 +97,17 @@ angular.module('playListApp', [])
 	self.user = new UserDTO;
 	self.music = new MusicDTO;
 
+	// Gather the genres into an array and set the default index to 0.
 	self.genreList = self.music.getGenres();
-	console.log(self.genreList);
-
-	self.welcome = "'s up";
-
-	self.updateSelection = function(boolean) {
-		console.log(boolean);
-	}
-
 	self.genreListIndex = 0;
 
-	self.selected = [];
-	console.log(self.music.masterList[0].songs[0]);
+	self.createPlayList = function() {
+		self.summary = self.music.addSong()
+	}
 
+	self.summary = [];
+
+	self.lyricType = "explicit";
+
+	self.welcome = "Welcome to 'S up Tempo";
 })

8c82e1b6049246a0c2398627f2a8a1d559d6ea23
Christopher Mehmed <chrismehmed@gmail.com>
2015-12-30 00:26:00 -0500

    board with buttons

--- HW2/index.js
+++ HW2/index.js
@@ -1,28 +1,9 @@
 angular.module('simonApp', [])
 
-.factory('UserDTO', function() {
-
-	function User() {
-		this.name = "";
-		this.age = "";
-		//this.playList = []
-	}
-
-	User.prototype.addUser = function(name, age) {
-		if ( name == undefined || age == undefined ) {
-			return alert('Please enter name and age.');
-		}
-		this.name = name;
-		this.age = age;
-	}
-
-	return User;
-})
-
 .factory('SimonDTO', function() {
 
 	function Simon() {
-		this.pallet = ["Red", "Blue", "Yellow", "Green"];
+		this.pallet = ["red", "blue", "yellow", "green"];
 		this.currentColor = "";
 		this.allColors = [];
 	}
@@ -30,24 +11,36 @@ angular.module('simonApp', [])
 	Simon.prototype.simonSays = function () {
 		num = this.pallet.length;
 		index = Math.ceil( Math.random() * num ) - 1;
-		console.log(this.pallet[index]);
+		console.log("simonSays() " + this.pallet[index]);
 		return this.pallet[index];
 	}
 
 	Simon.prototype.addColorToList = function() {
 		this.allColors.push(color);
+		console.log("addColorToList()", this.allColors);
+		return this.allColors;
 	}
 
 	return Simon;
+})
+
+.factory('PlayerDTO', function() {
+
 })
 
 .controller('mainController', function(SimonDTO) {
 	var self = this;
 	self.simon = new SimonDTO;
 
-	console.log(self.simon);
+	self.test = function() {
+		color = self.simon.simonSays();
+		self.simon.addColorToList();
+	}
 
-	self.simon.simonSays();
+	self.playerPress = function(color) {
+		self.playerColor = color;
+		return color;
+	}
 
 	self.welcome = "Simon Says";
 
